21560e28a15596cfef5d59ffddfc2d2565d5e864
Niranjan-P-coder <28pillain@students.bcsc.k12.in.us>
2024-12-09 09:49:00 -0500

    Created tictactoe.py
    Created 2D list Gameboard and setup other functions & variables

---
+++ tictactoe.py
@@ -0,0 +1,19 @@
+turn = True
+
+# above grid represents coordinate system, top left is (0,0) bottom right is (2,2)
+gameboard = [["X","O ","O "],
+             ["X","O","X"], 
+             ["O","X","O"]]
+print(" " + gameboard[0][0] +  " | " + gameboard[0][1] + "| " + gameboard[0][2] + " ")
+print("-----------")
+print(" " + gameboard[1][0] +  " | " + gameboard[1][1] + " | " + gameboard[1][2] + " ")
+print("-----------")
+print(" " + gameboard[2][0] +  " | " + gameboard[2][1] + " | " + gameboard[2][2] + " ")
+#check board for winners
+def check_board():
+    pass
+#creates new board per turn
+def new_turn():
+    pass
+def reset_game():
+    pass
