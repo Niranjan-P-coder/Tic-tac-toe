834e90cd23006e0def446fdab57f63a0a3fbfb57
Niranjan-P-coder <28pillain@students.bcsc.k12.in.us>
2024-12-16 09:50:46 -0500

    Update tictactoe.py to Step 2

--- tictactoe.py
+++ tictactoe.py
@@ -1,14 +1,16 @@
 turn = True
+game = True
 
 # above grid represents coordinate system, top left is (0,0) bottom right is (2,2)
-gameboard = [["X","O ","O "],
-             ["X","O","X"], 
-             ["O","X","O"]]
-print(" " + gameboard[0][0] +  " | " + gameboard[0][1] + "| " + gameboard[0][2] + " ")
-print("-----------")
-print(" " + gameboard[1][0] +  " | " + gameboard[1][1] + " | " + gameboard[1][2] + " ")
-print("-----------")
-print(" " + gameboard[2][0] +  " | " + gameboard[2][1] + " | " + gameboard[2][2] + " ")
+gameboard = [[" "," "," "],
+             [" "," "," "], 
+             [" "," "," "]]
+def print_board():
+    print(" " + gameboard[0][0] +  " | " + gameboard[0][1] + " | " + gameboard[0][2] + " ")
+    print("-----------")
+    print(" " + gameboard[1][0] +  " | " + gameboard[1][1] + " | " + gameboard[1][2] + " ")
+    print("-----------")
+    print(" " + gameboard[2][0] +  " | " + gameboard[2][1] + " | " + gameboard[2][2] + " ")
 #check board for winners
 def check_board():
     pass
@@ -17,3 +19,22 @@ def new_turn():
     pass
 def reset_game():
     pass
+def take_input():
+    global turn
+    global gameboard
+    global game
+    user_input_row = input("Enter a value from 0 to 2 inclusive as : ")
+    x = int(user_input_row.strip())
+    #print(x)
+    user_input_column = input("Enter a value from 0 to 2 inclusive: ")
+    y = int(user_input_column.strip())
+    #print(y)
+    if turn: 
+        gameboard[x][y] ="X"
+    else:
+        gameboard[x][y] = "O"
+    turn = not turn
+while True:
+    take_input() 
+    #check wins
+    print_board()
